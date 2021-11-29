d555353998c86e0fc7295d5f778719242341c16f
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-28 19:02:37 -0700

    set up roles and employees schema and seeds

--- db/db.sql
+++ db/db.sql
@@ -1,2 +1,6 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
 CREATE DATABASE monstersinc;
 USE monstersinc;
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,24 @@
 -- CREATE TABLE FOR DEPARTMENTS
 CREATE TABLE departments (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
+);
+
+-- CREATE TABLE FOR ROLES
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+-- CREATE TABLE FOR EMPLOYEES
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    manager_id INT
 );
